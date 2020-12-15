34b75891c277f4d9fe6ab3f8bb268ce42b451a46
Marco.seb <marco.seba96@gmail.com>
2020-12-15 18:49:01 +0100

    Insertion code js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-alert('ciao');
+
+var element = document.getElementById("genera");
+element.addEventListener('click',
+function() {
+        var nomeUtente = document.getElementById('nome').value;
+        console.log(nomeUtente);
+        var distanza = document.getElementById('km').value;
+        console.log(distanza);
+        var eta = document.getElementById('eta').value;
+        console.log(eta);
+        
+        
+        var biglietto;
+        var prezzoTotale = distanza * 0.21;
+        
+        
+        biglietto = prezzoTotale;
+        
+        if (eta < 18){
+            biglietto = prezzoTotale - (prezzoTotale * 20 / 100);
+        }   else if (eta > 65) {
+            biglietto = prezzoTotale - (prezzoTotale * 40 / 100);
+        }
+
+        document.getElementById('name').innerHTML = nomeUtente;
+        document.getElementById('costo').innerHTML = biglietto;
+    }
+);
+
